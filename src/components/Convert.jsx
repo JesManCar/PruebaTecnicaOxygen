eeb523dabf9028ceea0417df0be7f1504a0ffa19
JesManCar <jesmancar99@gmail.com>
2024-04-19 19:50:33 +0200

    Convert - Correct Css and functionaly

--- src/components/Convert.jsx
+++ src/components/Convert.jsx
@@ -4,20 +4,30 @@ export default function Convert() {
 
     //let valueInput="";
 
-    const getInitialState = () => {
-        const value = "KM-ML";
+    const getInitialState = (param) => {
+        const value = param;
         return value;
       };
-    const [selectorValue, setOption] = useState(getInitialState());
+    const getInitialStateUnits = () => {
+        const value = "km";
+        return value;
+      };
+
+    const [selectorValue, setOption] = useState(getInitialState(0));
+
     const selectorChange = (event) => {
             setOption(event.target.value);
             setValueInput("");
             setCalculated("");
+            setUnits(event.target.value[0].toLowerCase()+event.target.value[1].toLowerCase());
+            setUnitsResults(event.target.value[3].toLowerCase()+event.target.value[4].toLowerCase());
     }
 
-    const regExpNumbers =/^[0-9]+$/;
+    const regExpNumbers =/^[0-9.]+$/;
     const [calculated, setCalculated] = useState();
     const [valueInput, setValueInput] = useState();
+    const [units, setUnits] = useState(getInitialState("km"));
+    const [unitsResult, setUnitsResults] = useState(getInitialState("ml"));
 
     const calculateConversion = (event) => {
         //setText(event.target.value);
@@ -27,22 +37,22 @@ export default function Convert() {
             
             switch(selectorValue) {
                 case 'KM-ML':
-                    setCalculated(event.target.value*0.621371);
+                    setCalculated((event.target.value*0.621371).toFixed(2));
                     break;
                 case 'ML-KM':
-                    setCalculated(event.target.value*1.60934);
+                    setCalculated((event.target.value*1.60934).toFixed(2));
                     break;
-                case 'FT-M':
-                    setCalculated(event.target.value*0.3048);
+                case 'FT-MT':
+                    setCalculated((event.target.value*0.3048).toFixed(2));
                     break;
-                case 'M-FT':
-                    setCalculated(event.target.value*3.28084);
+                case 'MT-FT':
+                    setCalculated((event.target.value*3.28084).toFixed(2));
                     break;
                 case 'CM-IN':
-                    setCalculated(event.target.value*0.3937008);
+                    setCalculated((event.target.value*0.3937008).toFixed(2));
                     break;
                 case 'IN-CM':
-                    setCalculated(event.target.value*2.54000008128);
+                    setCalculated((event.target.value*2.54000008128).toFixed(2));
                     break;
                 default:
                     setCalculated(event.target.value);
@@ -51,24 +61,35 @@ export default function Convert() {
         }
     }
 
+    const reverseConversion = (event) => {
+        let aux = valueInput;
+        let newOption = selectorValue[3]+selectorValue[4]+"-"+selectorValue[0]+selectorValue[1]
+        setOption(newOption);
+        setValueInput(calculated);
+        setCalculated(aux);
+        setUnits(selectorValue[0].toLowerCase()+selectorValue[1].toLowerCase());
+        setUnitsResults(selectorValue[3].toLowerCase()+selectorValue[4].toLowerCase());
+    }
+
   return (
-    <div className='grid'>
+    <div className='convert'>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <div className='box'>
-        <h1>convert</h1>
-        <select id="selector" value={selectorValue} onChange={selectorChange}>
+        <p className="tittle grid-item">convert</p>
+            <select className="grid-item large" id="selector" value={selectorValue} onChange={selectorChange}>
             <option value="KM-ML">Kilómetros a Millas</option>
             <option value="ML-KM">Millas a Kilómetros</option>
-            <option value="FT-M">Pies a Metros</option>
-            <option value="M-FT">Metros a Pies</option>
+            <option value="MT-FT">Metros a Pies</option>
+            <option value="FT-MT">Pies a Metros</option>
             <option value="CM-IN">Centimetros a Pulgadas</option>
             <option value="IN-CM">Pulgadas a Centimetros</option>
         </select>
-        <span class="material-symbols-outlined">sync_alt</span>
-        <input value={valueInput} onChange={calculateConversion}></input>
-        <p>km</p>
-        <span class="material-symbols-outlined">favorite</span>
-        <p>{calculated}</p>
+        <span class="material-symbols-outlined fakeButton grid-item" onClick={reverseConversion}>sync_alt</span>
+        <input className="grid-item large text-right" value={valueInput} onChange={calculateConversion}></input>
+        <p className="grid-item units" value={units}>{units}</p>   
+        <span class="material-symbols-outlined fakeButton grid-item" /*onClick={saveConversion}*/>favorite</span>
+        <p className="grid-item result text-right" >{calculated || 0 }</p>
+        <p className="grid-item units" >{unitsResult}</p>
 
     </div>
     </div>
